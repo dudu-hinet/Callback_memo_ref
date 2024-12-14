0a42f3e269687222fa3edc2a32251951999e2041
LiangDu <26771549+dudu-hinet@users.noreply.github.com>
2024-12-14 23:42:40 +0800

    [fix] too many open files, watch

--- metro.config.js
+++ metro.config.js
@@ -5,13 +5,22 @@
  * @format
  */
 
+// [fix: too many open files, watch]https://github.com/expo/expo/issues/29083
+const exclusionList = require('metro-config/src/defaults/exclusionList')
 module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+	resolver: {
+		blacklistRE: exclusionList([
+			// /node_modules\/.*/,
+			/android\/.*/,
+			/ios\/.*/
+		]),
+	},
+	transformer: {
+		getTransformOptions: async () => ({
+			transform: {
+				experimentalImportSupport: false,
+				inlineRequires: true,
+			},
+		}),
+	},
+}
